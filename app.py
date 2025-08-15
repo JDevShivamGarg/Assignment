148b486781192c6cb56cd1bc26282501b5defbf3
Shivam <loyalgamer87@gmail.com>
2025-08-15 15:16:05 +0530

    removed notebook

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/notebook')
-def notebook():
-    return render_template('Assignment1Notebook.html')
 
 @app.route('/graphs')
 def graphs():
